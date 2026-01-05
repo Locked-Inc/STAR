aaad8dd809d012af13bbb42218eb78ae3ca10290
Brighton Sikarskie <bsikar@tuta.io>
2026-01-05 00:29:50 -0600

    refactor: Use pre-computed port/pin constants in hardware_pinout.h
    Replaces manual bit-shift operations with pre-computed constants from
    rx_port_constants.h for cleaner, more readable code.
    
    Before:
      k_gpio_pb2 = (k_rx_port_b << k_port_shift) | k_rx_pin_2,
    
    After:
      k_gpio_pb2 = k_rx_pb_2,
    
    Changes:
    - Updated all 63 gpio_pin_t definitions to use k_rx_pX_Y constants
    - Removes repetitive bit-shift operations (63 instances)
    - Improves readability and maintainability
    - No functional changes - constants evaluate to same values
    
    Verification:
    - All 11/11 unit tests pass
    - Compile-time constant evaluation ensures no performance impact

--- star-rx72n-firmware/include/hardware_pinout.h
+++ star-rx72n-firmware/include/hardware_pinout.h
@@ -58,108 +58,108 @@ extern "C" {
  */
 typedef enum {
   /* Port 0 GPIO Pins */
-  k_gpio_p05 = (k_rx_port_0 << k_port_shift) | k_rx_pin_5, /**< P05 (pin 100) - 1-Wire Temperature Sensor (DS18B20) */
-  k_gpio_p07 = (k_rx_port_0 << k_port_shift) | k_rx_pin_7, /**< P07 (pin 98) - PMOD JF GPIO1 */
+  k_gpio_p05 = k_rx_p0_5, /**< P05 (pin 100) - 1-Wire Temperature Sensor (DS18B20) */
+  k_gpio_p07 = k_rx_p0_7, /**< P07 (pin 98) - PMOD JF GPIO1 */
 
   /* Port 1 GPIO Pins */
-  k_gpio_p12 = (k_rx_port_1 << k_port_shift) | k_rx_pin_2, /**< P12 (pin 34) - BMS I2C SCL */
-  k_gpio_p13 = (k_rx_port_1 << k_port_shift) | k_rx_pin_3, /**< P13 (pin 33) - BMS I2C SDA */
-  k_gpio_p14 = (k_rx_port_1 << k_port_shift) | k_rx_pin_4, /**< P14 (pin 32) - Encoder 1 Phase A */
-  k_gpio_p15 = (k_rx_port_1 << k_port_shift) | k_rx_pin_5, /**< P15 (pin 31) - Encoder 0 Phase B */
-  k_gpio_p16 = (k_rx_port_1 << k_port_shift) | k_rx_pin_6, /**< P16 (pin 30) */
-  k_gpio_p17 = (k_rx_port_1 << k_port_shift) | k_rx_pin_7, /**< P17 (pin 29) - Motor 0 SPI CS */
+  k_gpio_p12 = k_rx_p1_2, /**< P12 (pin 34) - BMS I2C SCL */
+  k_gpio_p13 = k_rx_p1_3, /**< P13 (pin 33) - BMS I2C SDA */
+  k_gpio_p14 = k_rx_p1_4, /**< P14 (pin 32) - Encoder 1 Phase A */
+  k_gpio_p15 = k_rx_p1_5, /**< P15 (pin 31) - Encoder 0 Phase B */
+  k_gpio_p16 = k_rx_p1_6, /**< P16 (pin 30) */
+  k_gpio_p17 = k_rx_p1_7, /**< P17 (pin 29) - Motor 0 SPI CS */
 
   /* Port 2 GPIO Pins */
-  k_gpio_p20 = (k_rx_port_2 << k_port_shift) | k_rx_pin_0, /**< P20 (pin 28) - PMOD JF I2C SDA */
-  k_gpio_p21 = (k_rx_port_2 << k_port_shift) | k_rx_pin_1, /**< P21 (pin 27) - PMOD JF I2C SCL */
-  k_gpio_p22 = (k_rx_port_2 << k_port_shift) | k_rx_pin_2, /**< P22 (pin 26) - Encoder 1 Phase B */
-  k_gpio_p23 = (k_rx_port_2 << k_port_shift) | k_rx_pin_3, /**< P23 (pin 25) - Motor 1 SPI CS */
-  k_gpio_p24 = (k_rx_port_2 << k_port_shift) | k_rx_pin_4, /**< P24 (pin 24) - Encoder 0 Phase A */
-  k_gpio_p25 = (k_rx_port_2 << k_port_shift) | k_rx_pin_5, /**< P25 (pin 23) - Encoder 3 Phase A */
-  k_gpio_p26 = (k_rx_port_2 << k_port_shift) | k_rx_pin_6, /**< P26 (pin 22) - JTAG TDO */
-  k_gpio_p27 = (k_rx_port_2 << k_port_shift) | k_rx_pin_7, /**< P27 (pin 21) - JTAG TCK */
+  k_gpio_p20 = k_rx_p2_0, /**< P20 (pin 28) - PMOD JF I2C SDA */
+  k_gpio_p21 = k_rx_p2_1, /**< P21 (pin 27) - PMOD JF I2C SCL */
+  k_gpio_p22 = k_rx_p2_2, /**< P22 (pin 26) - Encoder 1 Phase B */
+  k_gpio_p23 = k_rx_p2_3, /**< P23 (pin 25) - Motor 1 SPI CS */
+  k_gpio_p24 = k_rx_p2_4, /**< P24 (pin 24) - Encoder 0 Phase A */
+  k_gpio_p25 = k_rx_p2_5, /**< P25 (pin 23) - Encoder 3 Phase A */
+  k_gpio_p26 = k_rx_p2_6, /**< P26 (pin 22) - JTAG TDO */
+  k_gpio_p27 = k_rx_p2_7, /**< P27 (pin 21) - JTAG TCK */
 
   /* Port 3 GPIO Pins */
-  k_gpio_p30 = (k_rx_port_3 << k_port_shift) | k_rx_pin_0, /**< P30 (pin 20) - JTAG TDI */
-  k_gpio_p31 = (k_rx_port_3 << k_port_shift) | k_rx_pin_1, /**< P31 (pin 19) - JTAG TMS */
-  k_gpio_p32 = (k_rx_port_3 << k_port_shift) | k_rx_pin_2, /**< P32 (pin 18) - Motor 2 SPI CS */
-  k_gpio_p33 = (k_rx_port_3 << k_port_shift) | k_rx_pin_3, /**< P33 (pin 17) - USB UART Reset */
-  k_gpio_p34 = (k_rx_port_3 << k_port_shift) | k_rx_pin_4, /**< P34 (pin 16) - JTAG TRST */
-  k_gpio_p35 = (k_rx_port_3 << k_port_shift) | k_rx_pin_5, /**< P35 (pin 15) - NMI/UPSEL */
-  k_gpio_p36 = (k_rx_port_3 << k_port_shift) | k_rx_pin_6, /**< P36 (pin 13) - EXTAL (24MHz Crystal) */
-  k_gpio_p37 = (k_rx_port_3 << k_port_shift) | k_rx_pin_7, /**< P37 (pin 11) - XTAL (24MHz Crystal) */
+  k_gpio_p30 = k_rx_p3_0, /**< P30 (pin 20) - JTAG TDI */
+  k_gpio_p31 = k_rx_p3_1, /**< P31 (pin 19) - JTAG TMS */
+  k_gpio_p32 = k_rx_p3_2, /**< P32 (pin 18) - Motor 2 SPI CS */
+  k_gpio_p33 = k_rx_p3_3, /**< P33 (pin 17) - USB UART Reset */
+  k_gpio_p34 = k_rx_p3_4, /**< P34 (pin 16) - JTAG TRST */
+  k_gpio_p35 = k_rx_p3_5, /**< P35 (pin 15) - NMI/UPSEL */
+  k_gpio_p36 = k_rx_p3_6, /**< P36 (pin 13) - EXTAL (24MHz Crystal) */
+  k_gpio_p37 = k_rx_p3_7, /**< P37 (pin 11) - XTAL (24MHz Crystal) */
 
   /* Port 4 GPIO Pins (ADC + nFAULT) */
-  k_gpio_p40 = (k_rx_port_4 << k_port_shift) | k_rx_pin_0, /**< P40 (pin 95) - Motor 0 Current ADC */
-  k_gpio_p41 = (k_rx_port_4 << k_port_shift) | k_rx_pin_1, /**< P41 (pin 93) - Motor 1 Current ADC */
-  k_gpio_p42 = (k_rx_port_4 << k_port_shift) | k_rx_pin_2, /**< P42 (pin 92) - Motor 2 Current ADC */
-  k_gpio_p43 = (k_rx_port_4 << k_port_shift) | k_rx_pin_3, /**< P43 (pin 91) - Motor 3 Current ADC */
-  k_gpio_p44 = (k_rx_port_4 << k_port_shift) | k_rx_pin_4, /**< P44 (pin 90) - Motor 0 nFAULT */
-  k_gpio_p45 = (k_rx_port_4 << k_port_shift) | k_rx_pin_5, /**< P45 (pin 89) - Motor 1 nFAULT */
-  k_gpio_p46 = (k_rx_port_4 << k_port_shift) | k_rx_pin_6, /**< P46 (pin 88) - Motor 2 nFAULT */
-  k_gpio_p47 = (k_rx_port_4 << k_port_shift) | k_rx_pin_7, /**< P47 (pin 87) - Motor 3 nFAULT */
+  k_gpio_p40 = k_rx_p4_0, /**< P40 (pin 95) - Motor 0 Current ADC */
+  k_gpio_p41 = k_rx_p4_1, /**< P41 (pin 93) - Motor 1 Current ADC */
+  k_gpio_p42 = k_rx_p4_2, /**< P42 (pin 92) - Motor 2 Current ADC */
+  k_gpio_p43 = k_rx_p4_3, /**< P43 (pin 91) - Motor 3 Current ADC */
+  k_gpio_p44 = k_rx_p4_4, /**< P44 (pin 90) - Motor 0 nFAULT */
+  k_gpio_p45 = k_rx_p4_5, /**< P45 (pin 89) - Motor 1 nFAULT */
+  k_gpio_p46 = k_rx_p4_6, /**< P46 (pin 88) - Motor 2 nFAULT */
+  k_gpio_p47 = k_rx_p4_7, /**< P47 (pin 87) - Motor 3 nFAULT */
 
   /* Port 5 GPIO Pins (PMOD JA + PMOD JB) */
-  k_gpio_p50 = (k_rx_port_5 << k_port_shift) | k_rx_pin_0, /**< P50 (pin 44) - PMOD JA COPI */
-  k_gpio_p51 = (k_rx_port_5 << k_port_shift) | k_rx_pin_1, /**< P51 (pin 43) - PMOD JA CIPO */
-  k_gpio_p52 = (k_rx_port_5 << k_port_shift) | k_rx_pin_2, /**< P52 (pin 42) - PMOD JA CS0 */
-  k_gpio_p53 = (k_rx_port_5 << k_port_shift) | k_rx_pin_3, /**< P53 (pin 41) - PMOD JA SCK */
-  k_gpio_p54 = (k_rx_port_5 << k_port_shift) | k_rx_pin_4, /**< P54 (pin 40) - PMOD JB GPIO2 */
-  k_gpio_p55 = (k_rx_port_5 << k_port_shift) | k_rx_pin_5, /**< P55 (pin 39) - PMOD JB GPIO1 */
+  k_gpio_p50 = k_rx_p5_0, /**< P50 (pin 44) - PMOD JA COPI */
+  k_gpio_p51 = k_rx_p5_1, /**< P51 (pin 43) - PMOD JA CIPO */
+  k_gpio_p52 = k_rx_p5_2, /**< P52 (pin 42) - PMOD JA CS0 */
+  k_gpio_p53 = k_rx_p5_3, /**< P53 (pin 41) - PMOD JA SCK */
+  k_gpio_p54 = k_rx_p5_4, /**< P54 (pin 40) - PMOD JB GPIO2 */
+  k_gpio_p55 = k_rx_p5_5, /**< P55 (pin 39) - PMOD JB GPIO1 */
 
   /* Port A GPIO Pins (SPI + PMOD) */
-  k_gpio_pa0 = (k_rx_port_a << k_port_shift) | k_rx_pin_0, /**< PA0 (pin 70) - Motor 3 SPI CS */
-  k_gpio_pa1 = (k_rx_port_a << k_port_shift) | k_rx_pin_1, /**< PA1 (pin 69) - PMOD JE GPIO3 */
-  k_gpio_pa2 = (k_rx_port_a << k_port_shift) | k_rx_pin_2, /**< PA2 (pin 68) - PMOD JB GPIO3 */
-  k_gpio_pa3 = (k_rx_port_a << k_port_shift) | k_rx_pin_3, /**< PA3 (pin 67) - Encoder 3 Phase B */
-  k_gpio_pa4 = (k_rx_port_a << k_port_shift) | k_rx_pin_4, /**< PA4 (pin 66) - RPi5 SPI CS */
-  k_gpio_pa5 = (k_rx_port_a << k_port_shift) | k_rx_pin_5, /**< PA5 (pin 65) - RPi5 SPI SCLK */
-  k_gpio_pa6 = (k_rx_port_a << k_port_shift) | k_rx_pin_6, /**< PA6 (pin 64) - RPi5 SPI COPI */
-  k_gpio_pa7 = (k_rx_port_a << k_port_shift) | k_rx_pin_7, /**< PA7 (pin 63) - RPi5 SPI CIPO */
+  k_gpio_pa0 = k_rx_pa_0, /**< PA0 (pin 70) - Motor 3 SPI CS */
+  k_gpio_pa1 = k_rx_pa_1, /**< PA1 (pin 69) - PMOD JE GPIO3 */
+  k_gpio_pa2 = k_rx_pa_2, /**< PA2 (pin 68) - PMOD JB GPIO3 */
+  k_gpio_pa3 = k_rx_pa_3, /**< PA3 (pin 67) - Encoder 3 Phase B */
+  k_gpio_pa4 = k_rx_pa_4, /**< PA4 (pin 66) - RPi5 SPI CS */
+  k_gpio_pa5 = k_rx_pa_5, /**< PA5 (pin 65) - RPi5 SPI SCLK */
+  k_gpio_pa6 = k_rx_pa_6, /**< PA6 (pin 64) - RPi5 SPI COPI */
+  k_gpio_pa7 = k_rx_pa_7, /**< PA7 (pin 63) - RPi5 SPI CIPO */
 
   /* Port B GPIO Pins (Debug UART + PMOD) */
-  k_gpio_pb0 = (k_rx_port_b << k_port_shift) | k_rx_pin_0, /**< PB0 (pin 61) - PMOD JE GPIO2 */
-  k_gpio_pb1 = (k_rx_port_b << k_port_shift) | k_rx_pin_1, /**< PB1 (pin 59) - PMOD JE GPIO1 */
-  k_gpio_pb2 = (k_rx_port_b << k_port_shift) | k_rx_pin_2, /**< PB2 (pin 58) - PMOD JA GPIO */
-  k_gpio_pb3 = (k_rx_port_b << k_port_shift) | k_rx_pin_3, /**< PB3 (pin 57) - PMOD JA RST */
-  k_gpio_pb4 = (k_rx_port_b << k_port_shift) | k_rx_pin_4, /**< PB4 (pin 56) - PMOD JA DC */
-  k_gpio_pb5 = (k_rx_port_b << k_port_shift) | k_rx_pin_5, /**< PB5 (pin 55) - PMOD JA CS1 */
-  k_gpio_pb6 = (k_rx_port_b << k_port_shift) | k_rx_pin_6, /**< PB6 (pin 54) - Debug UART RX */
-  k_gpio_pb7 = (k_rx_port_b << k_port_shift) | k_rx_pin_7, /**< PB7 (pin 53) - Debug UART TX */
+  k_gpio_pb0 = k_rx_pb_0, /**< PB0 (pin 61) - PMOD JE GPIO2 */
+  k_gpio_pb1 = k_rx_pb_1, /**< PB1 (pin 59) - PMOD JE GPIO1 */
+  k_gpio_pb2 = k_rx_pb_2, /**< PB2 (pin 58) - PMOD JA GPIO */
+  k_gpio_pb3 = k_rx_pb_3, /**< PB3 (pin 57) - PMOD JA RST */
+  k_gpio_pb4 = k_rx_pb_4, /**< PB4 (pin 56) - PMOD JA DC */
+  k_gpio_pb5 = k_rx_pb_5, /**< PB5 (pin 55) - PMOD JA CS1 */
+  k_gpio_pb6 = k_rx_pb_6, /**< PB6 (pin 54) - Debug UART RX */
+  k_gpio_pb7 = k_rx_pb_7, /**< PB7 (pin 53) - Debug UART TX */
 
   /* Port C GPIO Pins (Encoder + PMOD) */
-  k_gpio_pc0 = (k_rx_port_c << k_port_shift) | k_rx_pin_0, /**< PC0 (pin 52) - Encoder 2 Phase B */
-  k_gpio_pc1 = (k_rx_port_c << k_port_shift) | k_rx_pin_1, /**< PC1 (pin 51) - Encoder 2 Phase A */
-  k_gpio_pc2 = (k_rx_port_c << k_port_shift) | k_rx_pin_2, /**< PC2 (pin 50) - PMOD JC I2C SCL */
-  k_gpio_pc3 = (k_rx_port_c << k_port_shift) | k_rx_pin_3, /**< PC3 (pin 49) - PMOD JC I2C SDA */
-  k_gpio_pc4 = (k_rx_port_c << k_port_shift) | k_rx_pin_4, /**< PC4 (pin 48) - PMOD JC INT1 */
-  k_gpio_pc5 = (k_rx_port_c << k_port_shift) | k_rx_pin_5, /**< PC5 (pin 47) - PMOD JC INT2 */
-  k_gpio_pc6 = (k_rx_port_c << k_port_shift) | k_rx_pin_6, /**< PC6 (pin 46) - PMOD JB GPIO0 */
-  k_gpio_pc7 = (k_rx_port_c << k_port_shift) | k_rx_pin_7, /**< PC7 (pin 45) */
+  k_gpio_pc0 = k_rx_pc_0, /**< PC0 (pin 52) - Encoder 2 Phase B */
+  k_gpio_pc1 = k_rx_pc_1, /**< PC1 (pin 51) - Encoder 2 Phase A */
+  k_gpio_pc2 = k_rx_pc_2, /**< PC2 (pin 50) - PMOD JC I2C SCL */
+  k_gpio_pc3 = k_rx_pc_3, /**< PC3 (pin 49) - PMOD JC I2C SDA */
+  k_gpio_pc4 = k_rx_pc_4, /**< PC4 (pin 48) - PMOD JC INT1 */
+  k_gpio_pc5 = k_rx_pc_5, /**< PC5 (pin 47) - PMOD JC INT2 */
+  k_gpio_pc6 = k_rx_pc_6, /**< PC6 (pin 46) - PMOD JB GPIO0 */
+  k_gpio_pc7 = k_rx_pc_7, /**< PC7 (pin 45) */
 
   /* Port D GPIO Pins (Motor SPI + PMOD JD + PMOD JF) */
-  k_gpio_pd0 = (k_rx_port_d << k_port_shift) | k_rx_pin_0, /**< PD0 (pin 86) - PMOD JF GPIO0 */
-  k_gpio_pd1 = (k_rx_port_d << k_port_shift) | k_rx_pin_1, /**< PD1 (pin 85) - Motor SPI COPI */
-  k_gpio_pd2 = (k_rx_port_d << k_port_shift) | k_rx_pin_2, /**< PD2 (pin 84) - Motor SPI CIPO */
-  k_gpio_pd3 = (k_rx_port_d << k_port_shift) | k_rx_pin_3, /**< PD3 (pin 83) - Motor SPI SCLK */
-  k_gpio_pd4 = (k_rx_port_d << k_port_shift) | k_rx_pin_4, /**< PD4 (pin 82) - PMOD JD GPIO3 */
-  k_gpio_pd5 = (k_rx_port_d << k_port_shift) | k_rx_pin_5, /**< PD5 (pin 81) - PMOD JD GPIO2 */
-  k_gpio_pd6 = (k_rx_port_d << k_port_shift) | k_rx_pin_6, /**< PD6 (pin 80) - PMOD JD GPIO1 */
-  k_gpio_pd7 = (k_rx_port_d << k_port_shift) | k_rx_pin_7, /**< PD7 (pin 79) - PMOD JD GPIO0 */
+  k_gpio_pd0 = k_rx_pd_0, /**< PD0 (pin 86) - PMOD JF GPIO0 */
+  k_gpio_pd1 = k_rx_pd_1, /**< PD1 (pin 85) - Motor SPI COPI */
+  k_gpio_pd2 = k_rx_pd_2, /**< PD2 (pin 84) - Motor SPI CIPO */
+  k_gpio_pd3 = k_rx_pd_3, /**< PD3 (pin 83) - Motor SPI SCLK */
+  k_gpio_pd4 = k_rx_pd_4, /**< PD4 (pin 82) - PMOD JD GPIO3 */
+  k_gpio_pd5 = k_rx_pd_5, /**< PD5 (pin 81) - PMOD JD GPIO2 */
+  k_gpio_pd6 = k_rx_pd_6, /**< PD6 (pin 80) - PMOD JD GPIO1 */
+  k_gpio_pd7 = k_rx_pd_7, /**< PD7 (pin 79) - PMOD JD GPIO0 */
 
   /* Port E GPIO Pins (Motor PWM) */
-  k_gpio_pe0 = (k_rx_port_e << k_port_shift) | k_rx_pin_0, /**< PE0 (pin 78) - Motor 2 EN */
-  k_gpio_pe1 = (k_rx_port_e << k_port_shift) | k_rx_pin_1, /**< PE1 (pin 77) - Motor 1 EN */
-  k_gpio_pe2 = (k_rx_port_e << k_port_shift) | k_rx_pin_2, /**< PE2 (pin 76) - Motor 0 EN */
-  k_gpio_pe3 = (k_rx_port_e << k_port_shift) | k_rx_pin_3, /**< PE3 (pin 75) - Motor 2 PH */
-  k_gpio_pe4 = (k_rx_port_e << k_port_shift) | k_rx_pin_4, /**< PE4 (pin 74) - Motor 1 PH */
-  k_gpio_pe5 = (k_rx_port_e << k_port_shift) | k_rx_pin_5, /**< PE5 (pin 73) - Motor 0 PH */
-  k_gpio_pe6 = (k_rx_port_e << k_port_shift) | k_rx_pin_6, /**< PE6 (pin 72) - Motor 3 EN */
-  k_gpio_pe7 = (k_rx_port_e << k_port_shift) | k_rx_pin_7, /**< PE7 (pin 71) - Motor 3 PH */
+  k_gpio_pe0 = k_rx_pe_0, /**< PE0 (pin 78) - Motor 2 EN */
+  k_gpio_pe1 = k_rx_pe_1, /**< PE1 (pin 77) - Motor 1 EN */
+  k_gpio_pe2 = k_rx_pe_2, /**< PE2 (pin 76) - Motor 0 EN */
+  k_gpio_pe3 = k_rx_pe_3, /**< PE3 (pin 75) - Motor 2 PH */
+  k_gpio_pe4 = k_rx_pe_4, /**< PE4 (pin 74) - Motor 1 PH */
+  k_gpio_pe5 = k_rx_pe_5, /**< PE5 (pin 73) - Motor 0 PH */
+  k_gpio_pe6 = k_rx_pe_6, /**< PE6 (pin 72) - Motor 3 EN */
+  k_gpio_pe7 = k_rx_pe_7, /**< PE7 (pin 71) - Motor 3 PH */
 
   /* Port J GPIO Pins */
-  k_gpio_pj3 = (k_rx_port_j << k_port_shift) | k_rx_pin_3, /**< PJ3 (pin 4) - PMOD JE GPIO0 */
-  k_gpio_pj5 = (k_rx_port_j << k_port_shift) | k_rx_pin_5, /**< PJ5 (pin 2) - JTAG/TDO */
+  k_gpio_pj3 = k_rx_pj_3, /**< PJ3 (pin 4) - PMOD JE GPIO0 */
+  k_gpio_pj5 = k_rx_pj_5, /**< PJ5 (pin 2) - JTAG/TDO */
 
   /* Aliases for common use cases (point to same enum values) */
   k_gpio_temp_sensor         = k_gpio_p05,  /**< 1-Wire Temperature Sensor (DS18B20+) */
