b2b7d913af2d10e88ba6f1a2f4e60ee2ec1cbf2b
Brighton Sikarskie <bsikar@tuta.io>
2026-01-08 15:44:30 -0600

    fmt

--- star-rx72n-firmware/lib/rx_core/src/rx_iwdt.c
+++ star-rx72n-firmware/lib/rx_core/src/rx_iwdt.c
@@ -376,8 +376,9 @@ rx_err_t rx_iwdt_check_tasks(void)
     }
 
     /* Calculate elapsed time */
-    uint32_t elapsed_ticks    = current_tick - s_iwdt_state.tasks[i].last_heartbeat_tick;
-    uint32_t timeout_in_ticks = (s_iwdt_state.tasks[i].timeout_ms * TX_TIMER_TICKS_PER_SECOND) / k_ms_per_second;
+    uint32_t elapsed_ticks = current_tick - s_iwdt_state.tasks[i].last_heartbeat_tick;
+    uint32_t timeout_in_ticks =
+      (s_iwdt_state.tasks[i].timeout_ms * TX_TIMER_TICKS_PER_SECOND) / k_ms_per_second;
 
     /* Check for timeout */
     if (elapsed_ticks > timeout_in_ticks) {
