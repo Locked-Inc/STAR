0caf6947b396713be989fcf022156cef906cb282
IMMZEK <47085903+IMMZEK@users.noreply.github.com>
2026-01-12 21:19:23 -0600

    feat(spi): implement StarSpiDriverNode lifecycle node

---
+++ star-ros2/src/star_spi_bridge/launch/star_spi_bridge.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch_ros.actions import LifecycleNode
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    # Declare arguments
+    spi_device_path_arg = DeclareLaunchArgument(
+        'spi_device_path', default_value='/dev/spidev0.0',
+        description='Path to SPI device'
+    )
+
+    # Node
+    driver_node = LifecycleNode(
+        package='star_spi_bridge',
+        executable='star_spi_driver_node',
+        name='star_spi_driver',
+        namespace='',
+        output='screen',
+        parameters=[{
+            'spi_device_path': LaunchConfiguration('spi_device_path'),
+            'spi_speed_hz': 10000000,
+            'cmd_vel_timeout_ms': 500,
+            'wheel_base': 0.150,
+            'wheel_radius': 0.0325,
+            'ticks_per_rev': 11599
+        }]
+    )
+
+    return LaunchDescription([
+        spi_device_path_arg,
+        driver_node
+    ])
