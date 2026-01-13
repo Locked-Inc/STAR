bf24ec3f9392e008fd69d848623ac8d191e6b4ca
IMMZEK <47085903+IMMZEK@users.noreply.github.com>
2026-01-12 22:58:34 -0600

    Ran formatting script

--- star-ros2/src/star_spi_bridge/include/star_spi_bridge/spi_message_converter.hpp
+++ star-ros2/src/star_spi_bridge/include/star_spi_bridge/spi_message_converter.hpp
@@ -3,8 +3,8 @@
 
 #include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
-#include <sensor_msgs/msg/joint_state.hpp>
 #include <sensor_msgs/msg/battery_state.hpp>
+#include <sensor_msgs/msg/joint_state.hpp>
 
 #include "star/v1/motor_control.pb.h"
 #include "star/v1/telemetry.pb.h"
@@ -13,38 +13,39 @@ namespace star_spi_bridge {
 
 class SpiMessageConverter {
 public:
-    struct Parameters {
-        double wheel_base;
-        double wheel_radius;
-        int32_t ticks_per_rev;
-    };
+  struct Parameters {
+    double  wheel_base;
+    double  wheel_radius;
+    int32_t ticks_per_rev;
+  };
 
-    explicit SpiMessageConverter(const Parameters& params);
+  explicit SpiMessageConverter(const Parameters& params);
 
-    // ROS2 -> Protobuf
-    bool twist_to_velocity_command(const geometry_msgs::msg::Twist& twist, star::v1::VelocityCommand& command);
+  // ROS2 -> Protobuf
+  bool twist_to_velocity_command(const geometry_msgs::msg::Twist& twist, star::v1::VelocityCommand& command);
 
-    // Protobuf -> ROS2
-    void telemetry_to_odometry(const star::v1::TelemetryData& telemetry, nav_msgs::msg::Odometry& odom);
-    void telemetry_to_joint_state(const star::v1::TelemetryData& telemetry, sensor_msgs::msg::JointState& joint_state);
-    void telemetry_to_battery_state(const star::v1::TelemetryData& telemetry, sensor_msgs::msg::BatteryState& battery_state);
+  // Protobuf -> ROS2
+  void telemetry_to_odometry(const star::v1::TelemetryData& telemetry, nav_msgs::msg::Odometry& odom);
+  void telemetry_to_joint_state(const star::v1::TelemetryData& telemetry, sensor_msgs::msg::JointState& joint_state);
+  void telemetry_to_battery_state(const star::v1::TelemetryData&  telemetry,
+                                  sensor_msgs::msg::BatteryState& battery_state);
 
 private:
-    Parameters params_;
-    
-    // Odometry state
-    double x_ = 0.0;
-    double y_ = 0.0;
-    double theta_ = 0.0;
-    
-    // Encoder state for delta calculation
-    int64_t prev_ticks_fl_ = 0;
-    int64_t prev_ticks_fr_ = 0;
-    int64_t prev_ticks_bl_ = 0;
-    int64_t prev_ticks_br_ = 0;
-    bool first_odom_msg_ = true;
+  Parameters params_;
 
-    static double normalize_angle(double angle);
+  // Odometry state
+  double x_     = 0.0;
+  double y_     = 0.0;
+  double theta_ = 0.0;
+
+  // Encoder state for delta calculation
+  int64_t prev_ticks_fl_  = 0;
+  int64_t prev_ticks_fr_  = 0;
+  int64_t prev_ticks_bl_  = 0;
+  int64_t prev_ticks_br_  = 0;
+  bool    first_odom_msg_ = true;
+
+  static double normalize_angle(double angle);
 };
 
 } // namespace star_spi_bridge
