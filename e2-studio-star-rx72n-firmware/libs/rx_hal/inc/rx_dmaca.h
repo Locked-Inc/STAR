2d543b5f8993bdd8baf41797cf25bcdac03fca59
jared-bartz-tamu <142908635+jared-bartz-tamu@users.noreply.github.com>
2026-02-28 22:19:10 -0600

    More Fixes
    yay we love fixes, not reviewed yet

--- e2-studio-star-rx72n-firmware/libs/rx_hal/inc/rx_dmaca.h
+++ e2-studio-star-rx72n-firmware/libs/rx_hal/inc/rx_dmaca.h
@@ -50,7 +50,7 @@
  * @author STAR Team
  * @date 2026-02-22
  * @version 1.0.0
- * @copyright Copyright (c) 2026 STAR Project. MIT License.
+ * @copyright Copyright (c) 2026 STAR Project
  */
 
 #pragma once
