1c1bea9c915e8514414ebcc7a23fb249bb70c4ae
CesMag <47085903+CesMag@users.noreply.github.com>
2026-03-04 10:51:18 -0600

    Adding STM32 project

---
+++ e2-studio-star-rx72n-firmware/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/STAR/e2-studio-star-rx72n-firmware
+# Build directory: /workspaces/STAR/e2-studio-star-rx72n-firmware/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
